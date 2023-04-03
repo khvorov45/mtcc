e78c2810dc7cfa5afb4ba0f34b8e262d36cc3c69
Arseniy Khvorov <khvorov45@gmail.com>
2023-04-03 18:40:18 +1000

    compile tcc

---
+++ build.c
@@ -0,0 +1,105 @@
+#include "cbuild.h"
+
+#define function static
+#define STR(x) prb_STR(x)
+#define LIT(x) prb_LIT(x)
+#define assert(x) prb_assert(x)
+
+typedef prb_Arena Arena;
+typedef prb_Bytes Bytes;
+typedef prb_Str   Str;
+typedef uint8_t   u8;
+typedef int32_t   i32;
+
+function prb_Status
+execCmd(Arena* arena, Str cmd) {
+    prb_writelnToStdout(arena, cmd);
+    prb_Process proc = prb_createProcess(cmd, (prb_ProcessSpec) {});
+    prb_Status  result = prb_launchProcesses(arena, &proc, 1, prb_Background_No);
+    return result;
+}
+
+int
+main() {
+    Arena  arena_ = prb_createArenaFromVmem(1 * prb_GIGABYTE);
+    Arena* arena = &arena_;
+
+    Str rootDir = prb_getParentDir(arena, STR(__FILE__));
+    Str tccDir = prb_pathJoin(arena, rootDir, STR("tinycc"));
+    Str tccIncludeDir = prb_pathJoin(arena, tccDir, STR("include"));
+
+    Str outDir = prb_pathJoin(arena, rootDir, STR("build"));
+    assert(prb_createDirIfNotExists(arena, outDir));
+    // assert(prb_clearDir(arena, outDir));
+
+    Str tccExe = prb_pathJoin(arena, outDir, STR("tcc.exe"));
+
+    // NOTE(khvorov) Compile TCC
+    {
+        Str libtcc1 = prb_pathJoin(arena, outDir, STR("libtcc1.a"));
+        // NOTE(khvorov) Run the c2str tool
+        {
+            Str defs_ = prb_pathJoin(arena, tccDir, STR("tccdefs_.h"));
+            if (!prb_isFile(arena, defs_)) {
+                Str file = prb_pathJoin(arena, tccDir, STR("conftest.c"));
+                Str out = prb_pathJoin(arena, tccDir, STR("c2str.exe"));
+                assert(execCmd(arena, prb_fmt(arena, "clang -DC2STR %.*s -o %.*s", LIT(file), LIT(out))));
+
+                Str defs = prb_pathJoin(arena, tccDir, STR("include/tccdefs.h"));
+                assert(execCmd(arena, prb_fmt(arena, "%.*s %.*s %.*s", LIT(out), LIT(defs), LIT(defs_))));
+            }
+        }
+
+        // NOTE(khvorov) Generate the config file
+        {
+            Str config = prb_pathJoin(arena, tccDir, STR("config.h"));
+            if (!prb_isFile(arena, config)) {
+                Str content = STR("#define CONFIG_TCC_PREDEFS 1\n#define TCC_VERSION \"0.9.27\"");
+                assert(prb_writeEntireFile(arena, config, content.ptr, content.len));
+            }
+        }
+
+        // NOTE(khvorov) Compile the main exe
+        {
+            if (!prb_isFile(arena, tccExe)) {
+                Str in = prb_pathJoin(arena, tccDir, STR("tcc.c"));
+                assert(execCmd(arena, prb_fmt(arena, "clang -DONE_SOURCE=1 -DCONFIG_TRIPLET=\"x86_64-linux-gnu\" -DTCC_TARGET_X86_64 %.*s -o %.*s", LIT(in), LIT(tccExe))));
+            }
+        }
+
+        // NOTE(khvorov) Compile libtcc1 (not to be confused with libtcc which is different and included in tcc.exe)
+        {
+            if (!prb_isFile(arena, libtcc1)) {
+                char* src[] = {
+                    "libtcc1.c",
+                    "alloca.S",
+                    "alloca-bt.S",
+                    "stdatomic.c",
+                    "atomic.S",
+                    "builtin.c",
+                    "tcov.c",
+                    "va_list.c",
+                    "dsohandle.c",
+                };
+
+                Str tccLibDir = prb_pathJoin(arena, tccDir, STR("lib"));
+
+                Str* allObjs = 0;
+                for (i32 ind = 0; ind < prb_arrayCount(src); ind++) {
+                    Str file = prb_pathJoin(arena, tccLibDir, STR(src[ind]));
+                    Str out = prb_replaceExt(arena, file, STR("o"));
+                    arrput(allObjs, out);
+                    assert(execCmd(arena, prb_fmt(arena, "%.*s -c -I%.*s %.*s -o %.*s", LIT(tccExe), LIT(tccIncludeDir), LIT(file), LIT(out))));
+                }
+
+                Str allObjsStr = prb_stringsJoin(arena, allObjs, arrlen(allObjs), STR(" "));
+                assert(execCmd(arena, prb_fmt(arena, "%.*s -ar rcs %.*s %.*s", LIT(tccExe), LIT(libtcc1), LIT(allObjsStr))));
+            }
+        }
+    }
+
+    // NOTE(khvorov) Test-run tcc
+    {
+        assert(execCmd(arena, prb_fmt(arena, "%.*s -I%.*s -L%.*s %s -o temp.exe", LIT(tccExe), LIT(tccIncludeDir), LIT(outDir), __FILE__)));
+    }
+}
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR=$(dirname "$0")
 RUN_BIN=$SCRIPT_DIR/build.exe
-clang -g -Wall -Wextra -I/usr/lib/llvm-14/include $SCRIPT_DIR/mtcc.c -o $RUN_BIN -lpthread -lclang && $RUN_BIN
+clang -g -Wall -Wextra -I/usr/lib/llvm-14/include $SCRIPT_DIR/build.c -o $RUN_BIN -lpthread -lclang && $RUN_BIN
